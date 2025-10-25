c50c967687891f20728a2b45d1707b9fea8bd953
Chalwk77 <jericho.crosby227@gmail.com>
2025-10-25 21:12:10 +1300

    initial commit

--- classes/SoundManager.lua
+++ classes/SoundManager.lua
@@ -1,34 +1,40 @@
+-- Luminex - Laser Puzzle
+-- License: MIT
+-- Copyright (c) 2025 Jericho Crosby (Chalwk)
+
+local newAudioSource = love.audio.newSource
+local playSound = love.audio.play
+
 local SoundManager = {}
 SoundManager.__index = SoundManager
 
 function SoundManager.new()
-    local instance = setmetatable({}, SoundManager)
+    local instance = setmetatable({
+        sounds = {
+            rotate = newAudioSource("assets/sounds/rotate.mp3", "static"),
+            connect = newAudioSource("assets/sounds/connect.mp3", "static"),
+            win = newAudioSource("assets/sounds/win.mp3", "static"),
+            background = newAudioSource("assets/sounds/background.mp3", "stream")
+        }
+    }, SoundManager)
 
-    instance.sounds = {
-        rotate = love.audio.newSource("assets/sounds/rotate.mp3", "static"),
-        connect = love.audio.newSource("assets/sounds/connect.mp3", "static"),
-        win = love.audio.newSource("assets/sounds/win.mp3", "static"),
-    }
-
-    -- Set volumes
     for _, sound in pairs(instance.sounds) do
-        sound:setVolume(0.7)
+        instance:setVolume(sound, 0.7)
     end
+
+    instance:play("background", true)
 
     return instance
 end
 
-function SoundManager:play(soundName)
-    if self.sounds[soundName] then
-        self.sounds[soundName]:stop()
-        self.sounds[soundName]:play()
-    end
+function SoundManager:play(soundName, loop)
+    if loop then self.sounds[soundName]:setLooping(true) end
+    self.sounds[soundName]:stop()
+    self.sounds[soundName]:play()
 end
 
-function SoundManager:setVolume(volume)
-    for _, sound in pairs(self.sounds) do
-        sound:setVolume(volume)
-    end
+function SoundManager:setVolume(sound, volume)
+    sound:setVolume(volume)
 end
 
 return SoundManager
